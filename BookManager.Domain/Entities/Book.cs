6c25006f33d224fe04831986e097f1b529c282bb
beatriz friso <beatriz.paula@me.com.br>
2022-11-16 19:26:53 -0300

    added entities

---
+++ BookManager.Domain/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BookManager.Domain.Entities.Base;
+
+namespace BookManager.Domain.Entities
+{
+    public class Book : Entity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Isbn { get; set; }
+        public string Language { get; set; }
+        public int Pages { get; set; }
+        public DateTime PublishDate { get; set; }
+        public Guid AuthorId { get; set; }
+        public Author Author { get; set; }
+        public Guid GenreId { get; set; }
+        public Genre Genre { get; set; }
+        
+        //acrescentar aqui uma lista de atributos para estudar as relações many-to-many no banco (dica do Matheus)
+    }
+}
